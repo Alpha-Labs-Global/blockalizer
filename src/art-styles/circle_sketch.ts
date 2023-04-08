c0327455df188ace7c2b8e5e0af56a7f9522913e
0xCodercrane <108444211+0xcodercrane@users.noreply.github.com>
2023-04-08 11:30:36 +0800

    clean up unused

--- src/art-styles/circle_sketch.ts
+++ src/art-styles/circle_sketch.ts
@@ -4,7 +4,6 @@
 import p5 from "p5";
 import p5Types, { Color } from "p5";
 import GenericSketch from "./generic_sketch";
-import Scribble from "./scribble";
 
 export interface CircleSketchOptions {
   numOfBoxes: number;
@@ -15,48 +14,23 @@ export interface CircleSketchOptions {
   paperIndex: number;
 }
 
-// const paper_links: Array<string> = [
-//   "https://maroon-petite-shrew-493.mypinata.cloud/ipfs/QmYPmQdJQJLtvMpbXgeHjWzatnyVyCMxcpQsknR8AbYuR6",
-//   "https://maroon-petite-shrew-493.mypinata.cloud/ipfs/QmXdCnGST3VXiBBGAtEGLu4h3WyjZhayLJD9d6cUYtdDaj",
-//   "https://maroon-petite-shrew-493.mypinata.cloud/ipfs/Qmc1UUU1dkfnKQNAtuZEq5QEDNpwAj4DR5N8eP2VFVxxgk",
-// ];
-
-const paper_links: Array<string> = [
-  "https://blockalizer-animation-template.s3.us-east-2.amazonaws.com/paper1.webp",
-  "https://blockalizer-animation-template.s3.us-east-2.amazonaws.com/paper2.webp",
-  "https://blockalizer-animation-template.s3.us-east-2.amazonaws.com/paper3.webp",
-];
-
 export class CircleSketch extends GenericSketch {
   backgroundColor: p5Types.Color; // background color to use if overridden
   sizeOfBox: number; // size in px of each individual square
   noFill: boolean; // setting it to false removes all color
   removeBlocks: number; // 0 means none, 1 is low, 2 is medium, 3 is high
-  blockOrientation: Array<number>; // Inform the shape of the block. 1 is ◣ and 0 is ◢.
   colorPalette: Array<Color>; // Color Palette to be used
   paletteIndex: number; // Selected index from color table
   gridSize: number; // Size of the grid
-  iterator: number; // iterate over bits
-  triangleIterator: number;
   margins: number; // margin around sketch;
   sketchWidth: number;
   sketchHeight: number;
-  allTriangles: Array<Array<Array<number>>>; // arrays of trianges to array of vertices to a pair
-  scribble: Scribble;
-  fillLines: Array<Array<Array<Array<Array<number | boolean>>>>>;
-  fillIterator: number;
-  lineIterator: number;
-  firstLine: boolean;
-  fillDone: boolean;
-  triangleFillColors: Array<p5Types.Color>;
   animate: boolean;
-  paper: p5Types.Image | null;
   strokeWidth: number; // stroke width to use
   strokeWidthOutline: number;
   strokeWidthFill: number;
   fillGap: number;
   strokeColor: p5.Color;
-  paperIndex: number; // 0,1,2
   artTypeRandNum: number; // 0, 1
 
   constructor(
@@ -79,15 +53,11 @@ export class CircleSketch extends GenericSketch {
     this.removeBlocks = opts.removeBlocks;
     this.animate = opts.animate;
 
-    const marginPct = 5;
     const squareSize = Math.min(this.canvasHeight, this.canvasWidth);
-    // this.margins = (squareSize * marginPct) / 100;
     this.margins = 0;
 
     this.sketchHeight = squareSize;
     this.sketchWidth = squareSize;
-    const lengthOfBlockOrientation = this.gridSize * this.gridSize;
-    this.blockOrientation = blockBitsDecomp.slice(0, lengthOfBlockOrientation);
 
     this.paletteIndex = opts.paletteIndex;
     this.printColors(this.paletteIndex);
@@ -176,20 +146,6 @@ export class CircleSketch extends GenericSketch {
     } else {
       this.strokeColor = this.p5.color(10);
     }
-
-    this.iterator = 1;
-    this.lineIterator = 0;
-    this.firstLine = true;
-    this.triangleIterator = 0;
-    this.allTriangles = [];
-    this.scribble = new Scribble(this.p5, bowing, roughness, maxOffset);
-    this.fillLines = [];
-    this.fillIterator = 0;
-    this.fillDone = true;
-    this.triangleFillColors = [];
-    this.paper = null;
-
-    this.paperIndex = opts.paperIndex;
   }
 
   setup(canvasParentRef: Element) {
@@ -198,27 +154,14 @@ export class CircleSketch extends GenericSketch {
       .parent(canvasParentRef);
 
     this.p5.frameRate(120); // highest possible framerate
-    this.p5.loadImage(
-      paper_links[this.paperIndex],
-      (img) => {
-        // console.log("paper image loaded");
-        // this.p5.image(img, 0, 0, this.canvasWidth, this.canvasHeight);
-        // this.p5.tint(50);
-        // this.p5.background(img);
-        this.preview();
-      },
-      (e) => {
-        console.log(e);
-      }
-    );
+    this.preview();
     this.p5.noLoop();
   }
 
   scaffolding() {
     console.log("> scaffolding: ");
     this.outline();
-    this.selectColors();
-    let bitIndex = 0;
+   let bitIndex = 0;
     for (
       let j = this.margins;
       j < this.sketchHeight - this.margins;
@@ -240,80 +183,6 @@ export class CircleSketch extends GenericSketch {
 
   preview() {
     this.scaffolding();
-    const strokeScale = this.strokeWidth;
-    const stokeRez = 0.02;
-
-    for (let i = 0; i < this.allTriangles.length; i++) {
-      // first draw all vertices
-      const vertices = this.allTriangles[i];
-      for (let j = 1; j < vertices.length; j++) {
-        const x1 = vertices[j - 1][0];
-        const y1 = vertices[j - 1][1];
-        const x2 = vertices[j][0];
-        const y2 = vertices[j][1];
-        let strokeOffset =
-          strokeScale *
-          this.p5.noise(x1 * stokeRez + 1000, y1 * stokeRez + 1000);
-        this.p5.stroke(this.strokeColor);
-        this.p5.strokeWeight(strokeOffset);
-        this.p5.beginShape();
-        this.p5.vertex(x1, y1);
-        this.p5.vertex(x2, y2);
-        this.p5.endShape();
-      }
-    }
-
-    // then fill them
-    for (let i = 0; i < this.fillLines.length; i++) {
-      const fillLines = this.fillLines[i];
-      const fillColor = this.triangleFillColors[i].toString();
-      this.p5.push();
-      this.p5.strokeWeight(this.strokeWidthFill);
-      this.p5.noFill();
-      this.p5.stroke(fillColor);
-      for (let j = 0; j < fillLines.length; j++) {
-        const lines = fillLines[j];
-
-        const firstLine = lines[0];
-        const secondLine = lines[1];
-        this.p5.beginShape();
-        for (let k = 0; k < firstLine.length; k++) {
-          const x = Number(firstLine[k][0]);
-          const y = Number(firstLine[k][1]);
-          const curve = firstLine[k][2];
-          if (curve) {
-            this.p5.curveVertex(x, y);
-          } else {
-            this.p5.vertex(x, y);
-          }
-        }
-        this.p5.endShape();
-
-        this.p5.beginShape();
-        for (let k = 0; k < secondLine.length; k++) {
-          const x = Number(secondLine[k][0]);
-          const y = Number(secondLine[k][1]);
-          const curve = secondLine[k][2];
-          if (curve) {
-            this.p5.curveVertex(x, y);
-          } else {
-            this.p5.vertex(x, y);
-          }
-        }
-        this.p5.endShape();
-      }
-      this.p5.pop();
-    }
-  }
-
-  selectColors() {
-    for (let j = 0; j < this.gridSize; j++) {
-      for (let i = 0; i < this.gridSize; i++) {
-        const noiseColor = this.p5.random();
-        const color = this.pickColors(noiseColor);
-        this.triangleFillColors.push(color);
-      }
-    }
   }
 
   generateCircles(i: number, j: number) {
@@ -424,7 +293,6 @@ export class CircleSketch extends GenericSketch {
     }
 
     this.p5.pop();
-    // this.allTriangles.push(triangleSteps);
   }
 
   generatePalette(paletteIndex: number) {
